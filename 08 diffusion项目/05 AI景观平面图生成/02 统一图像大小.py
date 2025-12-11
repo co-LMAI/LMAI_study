6fc12da9e632a4287da9717b96f78522b09ebc7a
Magic Mai <498269108@qq.com>
2025-12-11 18:20:49 +0800

    1211

--- 08 diffusion项目/05 AI景观平面图生成/02 统一图像大小.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-file_path = r"E:\Desktop\data\image"
-text_path = r"E:\Desktop\data\text"
-
-for filename in os.listdir(file_path):
-    image_path = os.path.join(file_path, filename)
